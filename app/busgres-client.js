9dc69efe76f26c35bc92ca3b932c07d990f1c610
Rana Salem <ranatasalem@gmail.com>
2024-04-25 22:29:05 +0100

    add methods for receiving and saving messages

--- app/busgres-client.js
+++ app/busgres-client.js
@@ -2,18 +2,48 @@ const { ServiceBusClient } = require('@azure/service-bus')
 const { Client } = require('pg')
 
 class BusgresClient {
-  constructor(sbClient, pgClient, sbEntity) {
+  constructor(sbClient, sbConfig, pgClient) {
     this.sbClient = new ServiceBusClient(sbClient)
-    this.pgClient = new Client(pgClient)
-    this.sbEntity = sbEntity
-    this.sender = null
+    this.sbConfig = sbConfig
     this.receiver = null
+    this.sender = null
+    this.pgClient = new Client(pgClient)
   }
 
   async init() {
     await this.pgClient.connect()
-    this.sender = this.sbClient.createSender(this.sbEntity)
-    this.receiver = this.sbClient.createReceiver(this.sbEntity)
+  }
+
+  async receiveMessages() {
+    const receiver = this.sbClient.createReceiver(this.sbConfig)
+    receiver.subscribe({
+      processMessage: async (message) => {
+        console.log(
+          'The following message was received from Service Bus:',
+          message.body
+        )
+        await receiver.completeMessage(message)
+      },
+      processError: async (error) => {
+        console.error('Error occurred:', error)
+      }
+    })
+  }
+
+  async saveMessageToDatabase(message) {
+    try {
+      const query = 'INSERT INTO messages (content) VALUES ($1)'
+      const values = [message.body]
+      await this.pgClient.query(query, values)
+      console.log('Message saved to the database')
+    } catch (error) {
+      console.error('Error saving message to the database:', error)
+    }
+  }
+
+  async close() {
+    await this.pgClient.end()
+    await this.sbClient.close()
   }
 }
 
