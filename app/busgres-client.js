169044a1709e55517bfdd988322d6549d8378591
Rana Salem <ranatasalem@gmail.com>
2024-05-10 20:39:44 +0100

    Configure Busgres client & methods (#3)

--- app/busgres-client.js
+++ app/busgres-client.js
@@ -2,18 +2,64 @@ const { ServiceBusClient } = require('@azure/service-bus')
 const { Client } = require('pg')
 
 class BusgresClient {
-  constructor(sbClient, pgClient, sbEntity) {
-    this.sbClient = new ServiceBusClient(sbClient)
+  constructor(sbConnectionString, sbConfig, pgClient) {
+    this.sbConnectionString = sbConnectionString
+    this.sbConfig = sbConfig
     this.pgClient = new Client(pgClient)
-    this.sbEntity = sbEntity
-    this.sender = null
-    this.receiver = null
   }
 
-  async init() {
+  async connect() {
     await this.pgClient.connect()
-    this.sender = this.sbClient.createSender(this.sbEntity)
-    this.receiver = this.sbClient.createReceiver(this.sbEntity)
+  }
+
+  async saveMessageToDatabase(tableName, columnNames, message) {
+    try {
+      const messageContent = message.body
+      const columns = columnNames
+        .map((column, index) => `$${index + 1}`)
+        .join(', ')
+      const query = `INSERT INTO ${tableName} (${columnNames.join(
+        ', '
+      )}) VALUES (${columns})`
+
+      const values = columnNames.map((column) => messageContent[column])
+
+      await this.pgClient.query(query, values)
+      console.log(
+        'The following message has been saved to the database:',
+        messageContent
+      )
+    } catch (error) {
+      console.error('Error saving message to the database:', error)
+    }
+  }
+
+  async receiveMessage(tableName, columnNames) {
+    this.sbClient = new ServiceBusClient(this.sbConnectionString)
+    this.receiver = this.sbClient.createReceiver(this.sbConfig)
+
+    this.receiver.subscribe({
+      processMessage: async (message) => {
+        console.log(
+          'The following message was received from Service Bus:',
+          message.body
+        )
+        await this.saveMessageToDatabase(tableName, columnNames, message)
+        await this.receiver.completeMessage(message)
+      },
+      processError: async (error) => {
+        console.error(
+          'Error occurred while receiving message from Service Bus:',
+          error
+        )
+      }
+    })
+  }
+
+  async disconnect() {
+    await this.pgClient.end()
+    await this.receiver.close()
+    await this.sbClient.close()
   }
 }
 
