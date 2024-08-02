64957934cbbc23a508958fbad3c82f4aa0a93eda
Rana Salem <ranatasalem@gmail.com>
2024-08-03 00:13:02 +0100

    test

--- app/busgres-client.js
+++ app/busgres-client.js
@@ -90,76 +90,51 @@
 //   BusgresClient
 // }
 
-const { ServiceBusClient } = require('@azure/service-bus')
+cconst { ServiceBusClient } = require('@azure/service-bus')
 const { Client } = require('pg')
 
-const BusgresClient = async (sbConnectionString, sbEntityName, sbEntityType, sbEntitySubscription, pgConfig) => {
+const BusgresClient = (sbConnectionString, sbEntityName, sbEntityType, sbEntitySubscription, pgConfig) => {
   const pgClient = new Client(pgConfig)
+  let sbClient, receiver
 
   const connect = async () => {
     try {
       await pgClient.connect()
+      sbClient = new ServiceBusClient(sbConnectionString)
+      if (sbEntityType === 'queue') {
+        receiver = sbClient.createReceiver(sbEntityName)
+      } else if (sbEntityType === 'topic' && sbEntitySubscription) {
+        receiver = sbClient.createReceiver(sbEntityName, sbEntitySubscription)
+      } else {
+        throw new Error('Invalid entity type (must be "queue" or "topic" OR missing subscription name for topic)')
+      }
     } catch (error) {
-      throw new Error('Error connecting to the database:', error)
+      throw new Error('Error connecting to the database or Service Bus:', error)
     }
   }
 
   const saveMessage = async (tableName, columnNames, message) => {
     try {
       const messageContent = message.body
-
-      const columns = columnNames
-        .map((column, index) => `$${index + 1}`)
-        .join(', ')
-
-      const query = `INSERT INTO ${tableName} (${columnNames.join(
-        ', '
-      )}) VALUES (${columns})`
-
+      const columns = columnNames.map((column, index) => `$${index + 1}`).join(', ')
+      const query = `INSERT INTO ${tableName} (${columnNames.join(', ')}) VALUES (${columns})`
       const values = columnNames.map((column) => messageContent[column])
-
       await pgClient.query(query, values)
-
-      console.log(
-        'The following message has been saved to the database:',
-        messageContent
-      )
+      console.log('The following message has been saved to the database:', messageContent)
     } catch (error) {
-      throw new Error('Error saving message to the database:', error)
+      console.error('Error saving message to the database:', error)
     }
   }
 
   const receiveMessage = async (tableName, columnNames) => {
-    const sbClient = new ServiceBusClient(sbConnectionString)
-    const receiver = sbClient.createReceiver(sbEntity)
-
-    if (sbEntityType === 'queue') {
-      const receiver = sbClient.createReceiver(sbEntityName)
-    } else if (sbEntityType === 'topic' && sbEntitySubscription) {
-      const receiver = sbClient.createReceiver(
-        sbEntityName,
-        sbEntitySubscription
-      )
-    } else {
-      throw new Error(
-        'Invalid entity type (must be "queue" or "topic" OR missing subscription name for topic'
-      )
-    }
-
     receiver.subscribe({
       processMessage: async (message) => {
-        console.log(
-          'The following message was received from Service Bus:',
-          message.body
-        )
+        console.log('The following message was received from Service Bus:', message.body)
         await saveMessage(tableName, columnNames, message)
         await receiver.completeMessage(message)
       },
       processError: async (error) => {
-        throw new Error(
-          'Error occurred while receiving message from Service Bus:',
-          error
-        )
+        console.error('Error occurred while receiving message from Service Bus:', error)
       }
     })
   }
@@ -167,10 +142,10 @@ const BusgresClient = async (sbConnectionString, sbEntityName, sbEntityType, sbE
   const disconnect = async () => {
     try {
       await pgClient.end()
-      await receiver.close()
-      await sbClient.close()
+      if (receiver) await receiver.close()
+      if (sbClient) await sbClient.close()
     } catch (error) {
-      throw new Error('Error disconnecting the Busgres client:', error)
+      console.error('Error disconnecting the Busgres client:', error)
     }
   }
 
@@ -178,7 +153,8 @@ const BusgresClient = async (sbConnectionString, sbEntityName, sbEntityType, sbE
     connect,
     saveMessage,
     receiveMessage,
-    disconnect
+    disconnect,
+    pgClient // Expose pgClient if needed directly
   }
 }
 
