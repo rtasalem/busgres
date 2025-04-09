5dc35c90e49cb0ce7438292f9e584d5bce8597c9
Rana Salem <ranatasalem@gmail.com>
2025-04-09 11:39:47 +0100

    bug fix - typo

--- app/messaging/receiver.js
+++ app/messaging/receiver.js
@@ -1,6 +1,6 @@
 import { ServiceBusClient } from '@azure/service-bus'
 
-export default async function createReceiver(connectionString, entityName, entityType, subscriptionName) {
+export default async function createReceiver (connectionString, entityName, entityType, subscriptionName) {
   const client = new ServiceBusClient(connectionString)
 
   let receiver
@@ -10,7 +10,7 @@ export default async function createReceiver(connectionString, entityName, entit
   } else if (entityType === 'topic' && subscriptionName) {
     receiver = client.createReceiver(entityName, subscriptionName)
   } else {
-    throw new Error('Invalid entity type (must be "queue" or "topic") OR subscriptionName is missing for topic")
+    throw new Error('Invalid entity type (must be "queue" or "topic") OR subscriptionName is missing for topic')
   }
 
   return { client, receiver }
